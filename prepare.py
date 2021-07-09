3cb9eb30344df8a866f499980049d216eb41ed69
Cindy Villanueva <cindy.villanueva.ds@gmail.com>
2021-07-09 16:14:28 -0500

    add functions

---
+++ prepare.py
@@ -0,0 +1,71 @@
+
+
+import pandas as pd
+import numpy as np
+import os
+import seaborn as sns
+
+# turn off pink warning boxes
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+
+
+
+
+def miss_dup_values(df):
+    '''
+    takes in a dataframe of observations and attributes and returns a dataframe where each row is an atttribute name, 
+    the first column is the number of rows with missing values for that attribute, 
+    and the second column is percent of total rows that have missing values for that attribute and 
+    duplicated rows.
+    '''
+        # Total missing values
+    mis_val = df.isnull().sum()
+        # Percentage of missing values
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+        #total of duplicated
+    dup = df.duplicated().sum()  
+        # Percentage of missing values
+    dup_percent = 100 * dup / len(df)
+        # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+    mis_val_table_ren_columns.iloc[:,1] != 0].sort_values('% of Total Values', ascending=False).round(1)
+        # Print some summary information
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+           "There are " + str(mis_val_table_ren_columns.shape[0]) +
+           " columns that have missing values.")
+    print( "  ")
+    print (f"** There are {dup} duplicate rows that represents {round(dup_percent, 2)}% of total Values**")
+        # Return the dataframe with missing information
+    return mis_val_table_ren_columns
+
+
+def split_data(df, target):
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames; stratify on churn.
+    
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[target])
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123, 
+                                       stratify=train_validate[target])
+    
+    
+    print(f'complete df -> {df.shape}')
+    print(f'train -> {train.shape}')
+    print(f'validate -> {validate.shape}')
+    print(f'test -> {test.shape}')
+
+    return train, validate, test
